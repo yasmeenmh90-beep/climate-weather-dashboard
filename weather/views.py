3aa786c382da3cddceb6d36906d2fd8d106b665f
Yasmeen_Azmat <yasmeenmh.90@gmail.com>
2026-07-25 17:01:59 +0400

    Use environment variable for OpenWeather API key
    Refactor API key retrieval to use environment variable.

--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,8 @@
 import requests
 from django.shortcuts import render
 
-API_KEY = "c0e380f1b4d6996639d2a61d48d49c92"
+import os
+API_KEY = os.environ.get("OPENWEATHER_API_KEY", "")
 
 def get_aqi_label(aqi):
     labels = {1: "Good 🟢", 2: "Fair 🟡", 3: "Moderate 🟠", 4: "Poor 🔴", 5: "Very Poor 🟣"}
